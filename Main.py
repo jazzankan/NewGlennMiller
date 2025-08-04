e2bb179e26132c12b35b77ee708f3e83af25a55a
anders <anders@webbsallad.se>
2025-08-04 14:13:30 +0200

    Färdig men inte kollad på riktigt.

--- Main.py
+++ Main.py
@@ -8,46 +8,43 @@ glenn_page = response.text
 months = ['januari', 'Januari', 'februari', 'Februari', 'mars', 'Mars','april', 'April', 'maj', 'Maj', 'juni', 'Juni','juli','Juli','augusti', 'Augusti', 'september', 'September', 'oktober', 'Oktober', 'november','November', 'december', 'December']
 found_months = []
 month_index = []
-
-print(glenn_page)
-"""
+artist_list = []
+date_list = []
+soup = BeautifulSoup(glenn_page, "html.parser")
+soup = soup.main
 for m in months:
     try:
-        month_index.append(glenn_page.index(m))
+        month_index.append(str(soup).index(m))
         found_months.append(m)
     except(ValueError):
         pass
 greatest_index = month_index.index(max(month_index))
 latest_month = found_months[greatest_index]
 print(f"Senaste månaden är { latest_month }:")
+glenn_page = str(soup)[month_index[-1]:]
+soup = BeautifulSoup(str(glenn_page), "html.parser")
+soup_artists = soup.find_all('p')
+soup_dates = soup.find_all('h2')
+for a in soup_artists:
+    #En del datum är p och inte h2
+    if not "20" in str(a):
+        artist_list.append(a.text.strip())
+    else:
+        date_list.append(a.text.strip())
 
-glenn_page = glenn_page[month_index[-1]:]
+for d in soup_dates:
+        date_list.append(d.text.strip())
 
-soup = BeautifulSoup(glenn_page, "html.parser")
-artist_clean = []
-date_clean = []
-artists = soup.find_all("p", class_="artist")
-for a in artists:
-    artist_text = a.get_text()
-    artist_clean.append(artist_text)
-    print(artist_text)
-
-dates = soup.find_all("p", class_="date")
-for d in dates:
-    date_text = d.get_text()
-    date_clean.append(date_text)
-
-if len(artist_clean) != len(date_clean):
+date_list.sort()
+if len(artist_list) != len(date_list):
     print("\nDiskrepans mellan artister och datum!")
 else:
-    user_input = input("Vill du mata in posterna? j/n:\n")
-    if user_input == "j":
-        artist_index = 0
-        date_index = 0
-        for ac in artist_clean:
-            feed_it(artist_clean[artist_index], date_clean[date_index])
-            print(artist_clean[artist_index])
-            artist_index += 1
-            date_index += 1
-            time.sleep(0.5)
-"""
+        for i in range(len(artist_list)):
+            print(f"{artist_list[i]}, {date_list[i]}")
+            user_input = input("Vill du mata in posten? j/n:\n")
+            if user_input == "j":
+                try:
+                    feed_it(artist_list[i],date_list[i])
+                    print("Posten inlagd.\n\n")
+                except:
+                    print(f"Misslyckad inmatning: {feed_it}")
